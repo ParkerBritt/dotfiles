3b4c921b2e12c05db503fc6ebea3b53e1fa7613b
bluejamm <thezombiezapper@gmail.com>
2023-08-13 10:45:42 +0100

    fixed path env var

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,14 +1,20 @@
 set fish_greeting
 source $HOME/.config/fish/env_vars.fish
-source $HOME/.config/fish/aliases.fish
+
+# User specific environment
+
+if not contains "$HOME/.local/bin" $PATH
+    set -gx PATH $HOME/.local/bin $HOME/bin $PATH
+end
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
-	starship init fish | source
-	
-	colorscript --exec panes
+    source $HOME/.config/fish/aliases.fish
+    starship init fish | source
 
-	source /usr/share/autojump/autojump.fish
+    colorscript --exec panes
+
+    source /usr/share/autojump/autojump.fish
 
 end
 
