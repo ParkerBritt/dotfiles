28b9bf8e56e950e412102ca915d5e65bf5da9912
bluejamm <thezombiezapper@gmail.com>
2023-09-03 15:12:35 +0100

    aliases: banner, aliases, ls --icons

--- fish/.config/fish/aliases.fish
+++ fish/.config/fish/aliases.fish
@@ -1,12 +1,12 @@
-alias la='ls -a'
+alias la='ls -a --icons'
 alias logout='qdbus org.kde.ksmserver /KSMServer logout 0 0 0'
-alias ll='exa -alh'
-alias llatest='exa -alh -s created -r'
+alias ll='exa -alh --icons'
+alias llatest='exa -alh -s created -r --icons'
 alias vim='nvim'
 alias top='bpytop'
 alias fetch='fastfetch --logo arch2'
 alias ..='cd ..'
-alias ls='exa'
+alias ls='exa --icons'
 alias files='nohup thunar > /dev/null 2>&1 &'
 alias vpn="sudo wg"
 alias vpn-on="sudo wg-quick up uk-lon"
@@ -17,6 +17,8 @@ alias clip="xsel -b"
 alias cal="calcure"
 alias dust-sum="dust -bd 0"
 alias film="cd /home/parker/Perforce/parker-arch/"
+alias banner='figlet -d ~/github/figlet-fonts/ -f "3d"'
+alias aliases="vim ~/.config/fish/aliases.fish"
 
 # backup
 alias back-remote="restic --repository-file ~/.config/restic/b2-credentials/repository --password-file ~/.config/restic/b2-credentials/key"
