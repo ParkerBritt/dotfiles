7366923c413ee16a432e7171e64289e728eaae83
bluejamm <thezombiezapper@gmail.com>
2023-09-12 19:02:00 +0100

    adjusted plugins

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -1,18 +1,15 @@
 return {
 	{
-		"numToStr/Comment.nvim",
-		 config = function()
-		 	require("Comment").setup()
-		 end
-	},
-	{
 		"nvim-lualine/lualine.nvim",
 		requires = { 'nvim-tree/nvim-web-devicons', opt = true },
 
 
+	},
+	{
+		"vladdoster/remember.nvim",
+		 config = function()
+		 	require("remember")
+		 end
 	}
-
-
-
 
 }
