199934dfb4b0b7a56f954d36a6b03d5ace18f723
parker <parker.g.britt@gmail.com>
2024-02-05 12:32:31 +0000

    idk

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -1,16 +1,15 @@
 return {
 	{
 		"nvim-lualine/lualine.nvim",
-		requires = { 'nvim-tree/nvim-web-devicons', opt = true },
+		requires = { "nvim-tree/nvim-web-devicons", opt = true },
 	},
-  {
-    "ethanholz/nvim-lastplace",
-	config = true,
-	opts = {
-		lastplace_ignore_buftype = {"quickfix", "nofile", "help"},
-		lastplace_ignore_filetype = {"gitcommit", "gitrebase", "svn", "hgcommit"},
-		lastplace_open_folds = true
+	{
+		"ethanholz/nvim-lastplace",
+		config = true,
+		opts = {
+			lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+			lastplace_ignore_filetype = { "gitcommit", "gitrebase", "svn", "hgcommit" },
+			lastplace_open_folds = true,
+		},
 	},
-
-  },
 }
