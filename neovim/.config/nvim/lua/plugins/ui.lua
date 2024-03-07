de75b8713364ba5eecffb74784349dd608354c83
parker <parker.g.britt@gmail.com>
2024-03-07 22:42:13 +0000

    fixed missing conform dependency switched from dashboard to alpha using josean martinez config

--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -110,15 +110,4 @@ return {
 	},
 
 	{ "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons" },
-
-	{
-		"nvimdev/dashboard-nvim",
-		event = "VimEnter",
-		config = function()
-			require("dashboard").setup({
-				-- config
-			})
-		end,
-		dependencies = { { "nvim-tree/nvim-web-devicons" } },
-	},
 }
