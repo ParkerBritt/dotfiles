0b20bb37d83205e2ce4d228cc575ae4ab1afbb68
parker <parker.g.britt@gmail.com>
2024-05-29 15:14:29 +0100

    temp removed flash

--- neovim/.config/nvim/lua/plugins/utils.lua
+++ neovim/.config/nvim/lua/plugins/utils.lua
@@ -17,26 +17,27 @@ return {
 		 end
 	},
 	-- flash
-	{
-	  "folke/flash.nvim",
-	  event = "VeryLazy",
-	  ---@type Flash.Config
-	  opts = {
-		modes = {
-			search = {
-				enabled = false,
-			},
-		},
-	  },
-	  -- stylua: ignore
-	  keys = {
-		{ "s", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash" },
-		{ "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-		{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-		{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-		{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-	  },
-	},
+	-- {
+	--   "folke/flash.nvim",
+	--   event = "VeryLazy",
+	--   ---@type Flash.Config
+	--   opts = {
+	-- 	modes = {
+	-- 		search = {
+	-- 			enabled = false,
+	-- 		},
+	-- 	},
+	--   },
+	--   -- stylua: ignore
+	--   keys = {
+	-- 	{ "s", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash" },
+	-- 	{ "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+	-- 	{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+	-- 	{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+	-- 	{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+	--   },
+	-- },
+
 	-- telescope
 	{
 		'nvim-telescope/telescope.nvim', tag = '0.1.2',
