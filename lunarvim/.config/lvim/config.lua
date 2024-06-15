35bddd9b1f8f02949641a47f0c368d1dbf69025a
parker <parker.g.britt@gmail.com>
2024-06-15 19:20:56 +0100

    add undotree to lvim

--- lunarvim/.config/lvim/config.lua
+++ lunarvim/.config/lvim/config.lua
@@ -1,3 +1,14 @@
 lvim.keys.normal_mode["L"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["H"] = ":BufferLineCyclePrev<CR>"
 vim.o.timeoutlen = 300
+
+lvim.plugins = {
+	{
+	  "jiaoshijie/undotree",
+	  dependencies = "nvim-lua/plenary.nvim",
+	  config = true,
+	  keys = { -- load the plugin only when using it's keybinding:
+		{ "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+	  },
+	},
+}
