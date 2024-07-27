5830531fcac69bd0c763943b60cbb0283dc15afc
Parker <parker@parkerbritt.com>
2024-07-26 22:14:25 -0700

    moved nix packages to separate file

--- nixos/flakes/configuration.nix
+++ nixos/flakes/configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./packages.nix
     ];
 
   # Bootloader.
@@ -96,19 +97,6 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    git
-    lunarvim
-    xclip
-    fastfetch
-    alacritty
-    fish
-    starship
-    wget
-    trash-cli
-    fastfetch
-    eza
-    unzip
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
