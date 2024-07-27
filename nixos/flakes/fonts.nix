539636a5bd887e21362bb5c5a5743b78f60a4ddc
Parker <parker@parkerbritt.com>
2024-07-26 22:53:34 -0700

    add fonts.nix

---
+++ nixos/flakes/fonts.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }:
+{
+    fonts.packages = with pkgs; [
+      # standard fonts
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      liberation_ttf
+      fira-code
+      fira-code-symbols
+      mplus-outline-fonts.githubRelease
+      dina-font
+      proggyfonts
+    
+      # nerdfonts
+      (nerdfonts.override { fonts = [
+        "FiraCode"
+        "DroidSansMono"
+        "Hermit"
+        "Meslo"
+      ];})
+    ];
+}
