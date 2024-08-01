2cb8680b0dab210a6934e8af67c4fb762958607a
Parker <parker@parkerbritt.com>
2024-08-01 13:11:53 -0700

    nix: switch back to fish as login shell

--- nixos/flakes/packages/fish.nix
+++ nixos/flakes/packages/fish.nix
@@ -5,13 +5,17 @@
     ];
     programs.fish.enable = true;
 
-    programs.bash = {
-      interactiveShellInit = ''
-        if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-        then
-          shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-          exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-        fi
-      '';
-    };
+    # login shell method
+    users.users.parker.shell= pkgs.fish;
+
+    # interactive shell method
+    # programs.bash = {
+    #   interactiveShellInit = ''
+    #     if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+    #     then
+    #       shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+    #       exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+    #     fi
+    #   '';
+    # };
 }
