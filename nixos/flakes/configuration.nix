d1f9b47b563cbe3e4d139c9b0ee3764a4979bc2b
Parker <parker@parkerbritt.com>
2024-08-03 00:01:48 -0700

    nix: add bluetooth

--- nixos/flakes/configuration.nix
+++ nixos/flakes/configuration.nix
@@ -27,6 +27,13 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
+  # enable bluetooth
+  hardware.bluetooth = {
+      enable = true; # enables support for Bluetooth
+      powerOnBoot = true; # powers up the default Bluetooth controller on boot
+  };
+  services.blueman.enable = true;
+
   # Set your time zone.
   time.timeZone = "America/Los_Angeles";
 
