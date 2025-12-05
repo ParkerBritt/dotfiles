8cff1cf23d4324904848a79bd8567aca6f35d0bd
parker <parker.g.britt@gmail.com>
2025-12-05 23:18:42 +0000

    feat: remove old neovim config

--- neovim/.config/nvim/lua/keybinds.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set('n', '<leader>bp', '<cmd>bn<cr>', {desc = "Go to next buffer"})
-vim.keymap.set('n', '<leader>bn', '<cmd>BufferLineCycleNext<CR>', { desc = "Cycle to next buffer" })
--- vim.keymap.set('n', '<leader>bn',  '<cmd>bp<cr>', {desc = "Go to previous buffer"})
