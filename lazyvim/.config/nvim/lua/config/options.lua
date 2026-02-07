6ab46480fbe8a6f79930b45205fa70c42a42a60b
parker <parker@parkerbritt.com>
2026-02-07 01:50:50 +0000

    feat(nvim): add formatter

--- lazyvim/.config/nvim/lua/config/options.lua
+++ lazyvim/.config/nvim/lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.autoformat = false
+vim.g.autoformat = true
 vim.opt.tabstop = 4 -- Set the number of columns for a tab
 vim.opt.shiftwidth = 4 -- Set the number of spaces per indentation level
 vim.opt.softtabstop = 4 -- Number of spaces that a <Tab> counts for while performing editing operations
---
+++ lazyvim/.config/nvim/lua/plugins/formatter.lua
@@ -0,0 +1,26 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    -- LazyVim will use these options when formatting with the conform.nvim formatter
+    default_format_opts = {
+      timeout_ms = 3000,
+      async = false, -- not recommended to change
+      quiet = false, -- not recommended to change
+    },
+    formatters_by_ft = {
+      lua = { "stylua" },
+      fish = { "fish_indent" },
+      sh = { "shfmt" },
+      python = { "black" },
+      json = { "jq" },
+    },
+    -- The options you set here will be merged with the builtin formatters.
+    -- You can also define any custom formatters here.
+    formatters = {
+      injected = { options = { ignore_errors = true } },
+      black = {
+        prepend_args = { "--line-length", "120" },
+      },
+    },
+  },
+}
