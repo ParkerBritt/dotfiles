af6d666c6473427a4bc9233e12f1fd594eafd231
parker <parker.g.britt@gmail.com>
2026-09-04 21:14:49 +0100

    fix(nvim): disable autoformatter for gitdiff

--- lazyvim/.config/nvim/lua/config/autocmds.lua
+++ lazyvim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,14 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Disable formatter for gitdiff
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    if vim.api.nvim_buf_get_name(args.buf):match("^diffview://") then
+      vim.schedule(function()
+        vim.lsp.buf_detach_client(args.buf, args.data.client_id)
+      end)
+    end
+  end,
+})
